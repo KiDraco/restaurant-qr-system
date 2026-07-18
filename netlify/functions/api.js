7b3805eee5cd3ded809738e7d5ac2fc9a9f9499a
LeandroC <leandro.conde@praxisemr.com>
2026-07-18 11:35:05 -0300

    fix: clean up debug code, keep stable path transformation middleware

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -6,18 +6,12 @@ const wrapper = express();
 
 // Middleware: ajusta el path de Netlify Functions al formato que Express espera
 wrapper.use((req, res, next) => {
-  // Debug: devolver el path original si es /debug
-  if (req.url === '/debug' || req.url === '/.netlify/functions/api/debug') {
-    return res.json({ originalUrl: req.url, url: req.url, method: req.method });
-  }
-
   const match = req.url.match(/^\/\.netlify\/functions\/api(\/.*)?$/);
   if (match) {
     const rest = match[1] || '/';
     req.url = rest === '/' ? '/' : '/api' + rest;
     req._parsedUrl = undefined;
   }
-
   next();
 });
 
