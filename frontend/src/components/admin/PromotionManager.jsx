7146adacbca2da022c714826267a5c6bcbc9ac32
LeandroC <leandro.conde@praxisemr.com>
2026-07-30 10:52:04 -0300

    fix: eslint dep array warning in PromotionManager

--- frontend/src/components/admin/PromotionManager.jsx
+++ frontend/src/components/admin/PromotionManager.jsx
@@ -33,6 +33,7 @@ export default function PromotionManager() {
 
   useEffect(() => {
     loadPromotions();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const showNotification = (type, message) => {
